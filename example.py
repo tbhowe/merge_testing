9a5b850acfca19cb6a1c49848576220c28b9bf35
Tim Howe <tbhowe@gmail.com>
2023-07-10 11:17:33 +0100

    new conflict

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import time
 
 # This is an example python script to test merge conflicts
-time.sleep(1)
+time.sleep(3)
 print(f"Hello World!, the time is {time.time()}")
-time.sleep(1)
+time.sleep(3)
